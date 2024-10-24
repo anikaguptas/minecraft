d9c77ef3c3578b26107fda68d96b81c65e34725d
Anika Gupta <anikaguptaindia@gmail.com>
2024-10-24 11:47:57 +0530

    add helper funcs

--- world.js
+++ world.js
@@ -4,18 +4,21 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 export class World extends THREE.Group {
   data = [];
-
+  //id means grass, rock , etc
+  //instance id is the instance mesh number
   constructor(size = { width: 32, height: 16 }) {
     super();
     this.size = size;
+    this.generateData();
   }
 
   generate() {
-    this.generateData();
     this.generateWorld();
   }
-
+  //generate terrain in the video
+  //assigns each block id to 1 that is grass
   generateData() {
+    this.data = [];
     for (let x = 0; x < this.size.width; x++) {
       const slice = [];
       for (let y = 0; y < this.size.height; y++) {
@@ -31,7 +34,6 @@ export class World extends THREE.Group {
       this.data.push(slice);
     }
   }
-
   generateWorld() {
     this.clear();
     let maxCount = this.size.width * this.size.width * this.size.height;
@@ -41,11 +43,50 @@ export class World extends THREE.Group {
     for (let x = 0; x < this.size.width; x++) {
       for (let z = 0; z < this.size.width; z++) {
         for (let y = 0; y < this.size.height; y++) {
-          matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
-          mesh.setMatrixAt(mesh.count++, matrix);
+          const blockid = this.getblock(x, y, z).id;
+          if (blockid != 0) {
+            const instanceId = mesh.count;
+            matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
+            mesh.setMatrixAt(instanceId, matrix);
+            this.setinstanceid(x, y, z, instanceId);
+            mesh.count++;
+          }
         }
       }
     }
     this.add(mesh);
+  }
+  getblock(x, y, z) {
+    if (this.inbounds(x, y, z)) {
+      return this.data[x][y][z];
+    } else {
+      return null;
+    }
+  }
+
+  setblockid(x, y, z, id) {
+    if (this.inbounds(x, y, z)) {
+      this.data[x][y][z].id = id;
+    }
+  }
+  setinstanceid(x, y, z, id) {
+    if (this.inbounds(x, y, z)) {
+      this.data[x][y][z].instanceId = id;
+    }
+  }
+  inbounds(x, y, z) {
+    if (
+      x >= 0 &&
+      x < this.size.width &&
+      y >= 0 &&
+      y < this.size.height &&
+      z >= 0 &&
+      z < this.size.width
+    ) {
+      return true;
+    } else {
+      console.log("Out of bounds");
+      return false;
+    }
   }
 }
